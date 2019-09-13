fa6899b051c1fbc72f2f5ef206d97a61bbb78709
Max Kokorin <kokorin.max@gmail.com>
2019-09-13 15:05:34 +0600

    attach signature to query fixes

--- service_def.go
+++ service_def.go
@@ -99,12 +99,13 @@ func (as *apiService) request(method string, endpoint string, params map[string]
 	if apiKey {
 		req.Header.Add("X-MBX-APIKEY", as.APIKey)
 	}
+
+	req.URL.RawQuery = q.Encode()
 	if sign {
 		level.Debug(as.Logger).Log("queryString", q.Encode())
-		q.Add("signature", as.Signer.Sign([]byte(q.Encode())))
+		req.URL.RawQuery = fmt.Sprintf("%s&signature=%s", req.URL.RawQuery, as.Signer.Sign([]byte(q.Encode())))
 		level.Debug(as.Logger).Log("signature", as.Signer.Sign([]byte(q.Encode())))
 	}
-	req.URL.RawQuery = q.Encode()
 
 	resp, err := client.Do(req)
 	if err != nil {
